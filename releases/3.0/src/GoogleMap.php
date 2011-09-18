31dcfb053791de234e49367debc511e73c7b94af
brad@mycnl.com <brad@mycnl.com@cbab8157-d029-44fe-aa60-8ed04a35951f>
2011-09-18 17:14:54 +0000

    Fix for http://code.google.com/p/php-google-map-api/issues/detail?id=8

--- releases/3.0/src/GoogleMap.php
+++ releases/3.0/src/GoogleMap.php
@@ -2504,8 +2504,7 @@ class GoogleMapAPI {
                 break;
             case 'YAHOO':
             default:        
-                $_url = 'http://%s/MapsService/V1/geocode';
-                $_url .= sprintf('?appid=%s&location=%s',$this->lookup_server['YAHOO'],$this->app_id,rawurlencode($address));
+                $_url = sprintf('http://%s/MapsService/V1/geocode?appid=%s&location=%s',$this->lookup_server['YAHOO'],$this->app_id,rawurlencode($address));
                 $_result = false;
                 if($_result = $this->fetchURL($_url)) {
                     preg_match('!<Latitude>(.*)</Latitude><Longitude>(.*)</Longitude>!U', $_result, $_match);
